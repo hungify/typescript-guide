6af323c26cf675d2f8a34db608d8de3273e307df
hungify <nmhung.dev@gmail.com>
2022-08-01 13:23:00 +0700

    #9: Improving OBJECT.KEYS

---
+++ src/tips/9-tip.ts
@@ -0,0 +1,22 @@
+// #9: Improving OBJECT.KEYS
+
+const myObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+//Issue
+Object.keys(myObject).forEach((key) => {
+  console.log(myObject[key]);
+  // Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{ a: number; b: number; c: number; }'.
+});
+
+// Solution
+export const ObjectKeys = <Obj>(obj: Obj): Array<keyof Obj> => {
+  return Object.keys(obj) as (keyof Obj)[];
+};
+
+ObjectKeys(myObject).forEach((key) => {
+  console.log(myObject[key]);
+});
